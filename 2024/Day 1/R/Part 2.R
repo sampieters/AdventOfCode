8a2ffb4a2f9a3be3b5ca987bee90deba420ac970
sampieters <sampieters@MacBook-Pro-van-Sam.local>
2024-12-04 12:37:44 +0100

    first day in R (could be more efficient)

---
+++ 2024/Day 1/R/Part 2.R
@@ -0,0 +1,15 @@
+PATH <- "./2024/Day 1/input.txt"
+
+# Load the text file
+data <- read.table(PATH, header=FALSE, sep=" ", stringsAsFactors = FALSE)
+
+left <- sort(data[,1])
+right <- sort(data[,4])
+
+result <- 0
+
+for(elem in left) {
+  result <- result + elem * length(which(right == elem))
+}
+
+print(result)
